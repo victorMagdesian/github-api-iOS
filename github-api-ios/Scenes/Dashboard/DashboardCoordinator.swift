5ccd5133195be264d625de6b6bdeacd2e504e4eb
Gabriel Matheus <gabriel.souza@vaivoa.com>
2021-12-07 10:58:10 -0300

    feat: cria tapped buttons

--- github-api-ios/Scenes/Dashboard/DashboardCoordinator.swift
+++ github-api-ios/Scenes/Dashboard/DashboardCoordinator.swift
@@ -2,16 +2,16 @@ import Foundation
 import UIKit
 
 class DashboardCoordinator: Coordinator {
-    
+
     let rootNavigationController = DetailViewController()
-    
+
     // ViewModel aqui
-    
+
     override func start() {
-        
+
     }
-    
+
     override func finish() {
     }
-    
+
 }
--- github-api-ios/Scenes/Dashboard/ViewControllers/FiltroViewController.swift
+++ github-api-ios/Scenes/Dashboard/ViewControllers/FiltroViewController.swift
@@ -8,18 +8,65 @@
 import UIKit
 
 class FiltroViewController: UIViewController {
+    @IBOutlet var starFilter: UIButton!
+    @IBOutlet var followFilter: UIButton!
+    @IBOutlet var dateFilter: UIButton!
+
+    @IBOutlet var ascendingFilter: UIButton!
+    @IBOutlet var descendingFilter: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let buttons: [UIButton] = [
+            starFilter,
+            followFilter,
+            dateFilter,
+            ascendingFilter,
+            descendingFilter
+        ]
+
+//        starFilter.addTarget(
+//            self,
+//            action: #selector(tappedButton),
+//            for: .touchUpInside
+//        )
+
+        for button in buttons {
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+
+            button.addTarget(
+                self,
+                action: #selector(tappedButton),
+                for: .touchUpInside
+            )
+
+            button.isHighlighted = false
+        }
+
     }
 
-    @IBAction func goToMainButton(_ sender: Any) {
+    @IBAction func goToMainButton(_ sender: UIButton) {
         print("Aperto o X")
     }
 
-    @IBAction func limparFiltroButton(_ sender: Any) {
+    @IBAction func limparFiltroButton(_ sender: UIButton) {
         print("Aperto o LIMPAR")
+    }
+
+    @objc func tappedButton(_ sender: UIButton) {
+        if sender.backgroundColor == .none {
+            sender.backgroundColor = .black
+            sender.layer.cornerRadius = 4
+            sender.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+            sender.setTitleColor(.white, for: .normal)
+            return
+        }
+
+        sender.backgroundColor = .none
+        sender.titleLabel?.font = UIFont.systemFont(ofSize: 12, weight: .semibold)
+        sender.titleLabel?.textColor = .black
+        sender.setTitleColor(.black, for: .normal)
     }
 
 }
