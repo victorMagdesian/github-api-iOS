f2522141e73a5c3bd0c8879923a85a2140c57529
Pedro Galdiano <pedrocastro014@gmail.com>
2021-12-12 12:27:11 -0300

    removendo warnings

--- github-api-ios/Resources/WebServices/GithubRepository.swift
+++ github-api-ios/Resources/WebServices/GithubRepository.swift
@@ -12,27 +12,26 @@ class GithubRepository {
     private let networkService = NetworkService()
     private let baseURLString = "https://api.github.com"
 
-//    func getRepositories(_ order: Repositories.OrderBy = .descending, page: Int = 1) -> Observable<Repositories> {
-//        let urlString = "\(baseURLString)/search/repositories?q=sort=stars&order=desc&per_page=100&page=\(page)"
-//        return networkService.execute(url: URL(string: urlString)!)
-//    }
-
     func getBranches(ownerName: String, repositoryName: String) -> Observable<[Branch]> {
         let urlString = baseURLString + "/repos/\(ownerName)/\(repositoryName)/branches"
         return networkService.execute(url: URL(string: urlString)!)
     }
+    
     func getCommits(ownerName: String, repositoryName: String) -> Observable<[Commit]> {
         let urlString = baseURLString + "/repos/\(ownerName)/\(repositoryName)/commits"
         return networkService.execute(url: URL(string: urlString)!)
     }
+    
     func getContributors(ownerName: String, repositoryName: String) -> Observable<[Contributor]> {
         let urlString = baseURLString + "/repos/\(ownerName)/\(repositoryName)/contributors"
         return networkService.execute(url: URL(string: urlString)!)
     }
+    
     func getReleases(ownerName: String, repositoryName: String) -> Observable<[Release]> {
         let urlString = baseURLString + "/repos/\(ownerName)/\(repositoryName)/contributors"
         return networkService.execute(url: URL(string: urlString)!)
     }
+    
     func getRepositoriesByName(
         repositoryName: String = "abc",
         _ sort: Repositories.FilterBy = .stars,
@@ -47,11 +46,9 @@ class GithubRepository {
 
         return networkService.execute(url: URL(string: url)!)
     }
+    
     func getReadme(ownerName: String, repositoryName: String) -> Observable<String> {
-        let urlString = "https://raw.githubusercontent.com/\(ownerName))/\(repositoryName))/master/README"
+        let urlString = "https://raw.githubusercontent.com/\(ownerName)/\(repositoryName)/master/README"
         return networkService.execute(url: URL(string: urlString)!)
       }
 }
-
-// abc - 435
-// bitcoin-abc - 1052
